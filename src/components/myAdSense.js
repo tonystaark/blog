29ed6da6e6d747b89f33c2aa9923d113bd30fac1
Tony Ng <ngkokhong.tony@gmail.com>
2019-05-21 08:04:33 +0800

    change ad id

--- src/components/myAdSense.js
+++ src/components/myAdSense.js
@@ -4,7 +4,7 @@ import AdSense from 'react-adsense';
 const MyAdSense = () => (
   <AdSense.Google
     client='ca-pub-7235408523384570'
-    slot='5705531085'
+    slot='9210081627'
     style={{ display: 'block' }}
     format='auto'
     responsive='true'
