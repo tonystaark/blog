d2c528edc6c2087a0da6be70b7d59f9df3cb13f6
Tony Ng <ngkokhong.tony@gmail.com>
2022-04-27 20:51:52 +0800

    revert recent commits

--- src/html.js
+++ src/html.js
@@ -3,37 +3,43 @@ import PropTypes from "prop-types"
 
 export default function HTML(props) {
   return (
-    <html {...props.htmlAttributes}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        {props.headComponents}
-         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        {/* <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-7235408523384570"
-            data-ad-slot="9210081627"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins> */}
-        <script dangerouslySetInnerHTML={{__html: '(window.adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-7235408523384570",enable_page_level_ads: true});'}}></script>
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
-        </noscript>
-        <div
-          key={`body`}
-          id="___gatsby"
-          dangerouslySetInnerHTML={{ __html: props.body }}
-        />
-        {props.postBodyComponents}
-      </body>
-    </html>
+    <>
+      <script
+        async
+        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <script>
+        {`
+        (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-7235408523384570",
+          enable_page_level_ads: true
+        });
+      `}
+      </script>
+      <html {...props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {props.headComponents}
+        </head>
+        <body {...props.bodyAttributes}>
+          {props.preBodyComponents}
+          <noscript key="noscript" id="gatsby-noscript">
+            This app works best with JavaScript enabled.
+          </noscript>
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: props.body }}
+          />
+          {props.postBodyComponents}
+        </body>
+      </html>
+    </>
   )
 }
 
